e98e0260ab4667d8f304a0ea9ceba20c35f0b10c
Justin W <justincweiler@gmail.com>
2025-12-23 15:16:03 -0800

    trackpad settings 2

--- home.nix
+++ home.nix
@@ -268,8 +268,8 @@
 					tap_button_map = "lrm";
 					click_method = "clickfinger";
 					clickfinger_button_map = "lrm";
-					accel_profile = "flat";
-					pointer_accel = "0.25";
+					accel_profile = "adaptive";
+					pointer_accel = "0";
 					dwt = "false";
 				};
 			};
